d928872da22436e08dcfd344db94d1b832e39ad0
wangchao <wangxiaochaocccc@163.com>
2023-06-22 17:54:42 +0800

    feat:pinia配置

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import router from './router'
 import App from './App.vue'
-
 import myLibs from './libs/index'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import '@/assets/styles/index.scss'
 
+const pinia = createPinia()
 const app = createApp(App)
+
 app.use(router)
+app.use(pinia)
 app.use(ElementPlus)
 app.use(myLibs)
 app.mount('#app')
